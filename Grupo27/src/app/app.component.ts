56dd75e5f8b0ef443997f26a2ee9b64ba6ef4cfc
Serlozano <90990906+Serlozano@users.noreply.github.com>
2021-11-11 08:33:09 -0500

    clase 10-11-2021

--- Grupo27/src/app/app.component.ts
+++ Grupo27/src/app/app.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+   
+  res:any;
+
+  contenido:any;
+
+  urlapi:string="http://universities.hipolabs.com/search?country=Colombia";
+
+  //FUNCION DE CONTROL DE ERRORES
+  handleError(error: HttpErrorResponse) {
+    let errorMessage ='Error desconocido!';
+    if (error.error instanceof ErrorEvent){
+      //Errores del lado del cliente
+      errorMessage= `Error: ${ error.error.message}\n ${error.status}`;  
+    } else {
+      //Errores del lado del servidor
+      errorMessage =`Codigo de Error: ${error.status}\nMensaje: ${error.message}`;
+
+    }
+    return throwError(errorMessage);
+  }
+  
+
+
+
+  suma: number=0;
+
+  constructor(private objetohttp:HttpClient){}
+  
+  ngOnInit(): void {
+    this.suma=45+98
+
+    //utilizando el servicio en la url
+    this.res=this.objetohttp.get(this.urlapi).pipe(catchError(this.handleError));
+    this.res.subscribe((datos:any[])=>{
+      this.contenido=datos;
+      console.log(this.contenido);
+    });
+  
+  }
+  
+  
+}
+  
+  
+  
+  /*
+  
   title = 'Login';
 
   listausuarios=["diego","sergio","admin","user"];
@@ -32,4 +82,5 @@ export class AppComponent {
     }
 
   }
-}
+  */
+
